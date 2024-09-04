7f1c067967a42fbde7cae86ab3598bbe481ce3d3
Tom-J <Tom-Jennes@web.de>
2024-09-04 16:21:37 +0200

    update

--- dot2.py
+++ dot2.py
@@ -131,7 +131,6 @@ def read_midi_messages(console=None):
                             elif work_buttons["delete"]:
                                 console.command(f"Delete Executor {page + 1}.{note - 8 + 101}")
                                 work_buttons[i] = False
-                                send_note(work_buttons_code[i], 0)
                                 send_note(work_buttons_code["delete"], 0)
                                 work_buttons["delete"] = False
                                 continue
@@ -193,7 +192,6 @@ def read_midi_messages(console=None):
                                 continue
                             elif work_buttons["delete"]:
                                 console.command(f"Delete Executor {page + 1}.{note - 16 + 201}")
-                                work_buttons[i] = False
                                 send_note(work_buttons_code["delete"], 0)
                                 work_buttons["delete"] = False
                                 continue
